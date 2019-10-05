a2fa05444202bbeff9b7f1e1ef61db2c8b886ec1
IsShai <45069507+IsShai@users.noreply.github.com>
2019-10-06 00:58:28 +0300

    first commit

---
+++ Damka.Logic/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Damka.Logic
+{
+   /* {
+    public class Program
+    {
+
+    }
+        // $G$ SFN-999 (-5) Player name should not be empty
+        // $G$ SFN-999 (-5) You don't show if player turn is 'X' or 'O'
+        // $G$ SFN-013 (-1) Score calculation does not show the loser score
+        public static void Main()
+        {
+            GameManager game = new GameManager();
+            game.Run();
+        }
+    }*/
+}
---
+++ DamkaUI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DamkaUI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            DamkaGame st = new DamkaGame();
+            st.ShowDialog();
+        }
+    }
+}
